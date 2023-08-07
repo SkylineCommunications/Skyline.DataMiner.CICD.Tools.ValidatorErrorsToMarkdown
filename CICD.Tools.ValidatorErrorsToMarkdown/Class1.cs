c55be65b378ef73f80c2414804b6226527bfc7c9
Jarne Vynckier <jarne.vynckier@skyline.be>
2023-08-07 14:23:57 +0200

    work in progress

--- CICD.Tools.ValidatorErrorsToMarkdown/Class1.cs
+++ CICD.Tools.ValidatorErrorsToMarkdown/Class1.cs
@@ -1,7 +1,16 @@
 ﻿namespace Skyline.DataMiner.CICD.Tools.ValidatorErrorsToMarkdown
 {
+	using System;
+	using System.Linq;
+
+	/// <summary>
+	/// 
+	/// </summary>
 	public class Class1
 	{
+		public void test()
+        {
 
+        }
 	}
 }
---
+++ CICD.Tools.ValidatorErrorsToMarkdown/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+
+using Skyline.DataMiner.CICD.FileSystem;
+
+namespace CICD.Tools.ValidatorErrorsToMarkdown
+{
+    internal class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var fileLocation = new Option<string>(
+                name: "--fileLocation",
+                description: "File containing the ErrorMessages.")
+            {
+                IsRequired = true
+            };
+            var resultLocation
+
+            var rootCommand = new RootCommand("Returns MarkDown from xml file.")
+            {
+                fileLocation,
+            };
+
+
+            rootCommand.SetHandler(Process, fileLocation);
+
+            await rootCommand.InvokeAsync(args);
+
+            return 0;
+        }
+
+        private static async Task Process(string fileLocation)
+        {
+            XDocument xml = XDocument.Load(fileLocation);
+        }
+    }
+}
